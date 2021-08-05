ed3fe4c60887ec2ab967a84b0e5da14a9d90e19b
Liangchen <leeonky@gmail.com>
2021-08-05 12:29:51 +0800

    Add homework feature

--- src/test/java/com/odde/atddv2/TestSteps.java
+++ src/test/java/com/odde/atddv2/TestSteps.java
@@ -1,7 +1,9 @@
 package com.odde.atddv2;
 
 import io.cucumber.java.After;
+import io.cucumber.java.zh_cn.假如;
 import io.cucumber.java.zh_cn.当;
+import io.cucumber.java.zh_cn.那么;
 import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -30,6 +32,26 @@ public class TestSteps {
         getWebDriver().get("http://host.docker.internal:10081/");
         assertThat(getWebDriver().findElements(xpath("//*[text()='登录']"))).isNotEmpty();
         getWebDriver().quit();
+    }
+
+    @假如("存在用户名为\"joseph\"和密码为\"{int}\"的用户")
+    public void 存在用户名为_joseph_和密码为_的用户(Integer int1) {
+    }
+
+    @当("通过API以用户名为\"joseph\"和密码为\"{int}\"登录时")
+    public void 通过api以用户名为_joseph_和密码为_登录时(Integer int1) {
+    }
+
+    @那么("打印Token")
+    public void 打印_token() {
+    }
+
+    @当("在百度搜索关键字\"cucumber\"")
+    public void 在百度搜索关键字_cucumber() {
+    }
+
+    @那么("打印百度为您找到的相关结果数")
+    public void 打印百度为您找到的相关结果数() {
     }
 
     private WebDriver getWebDriver() {
---
+++ src/test/resources/homework.feature
@@ -0,0 +1,23 @@
+# language: zh-CN
+功能: 熟悉环境
+  作业1：熟悉 Cucumber、JPA、OkHttp 库使用。
+  其中 `假如存在用户名为"joseph"和密码为"123"的用户` 使用 Spring Boot Data JPA 把用户名密码数据写入数据库的 users 表
+  其中 `当通过API以用户名为"joseph"和密码为"123"登录时` 使用 OkHttp 发送 POST 请求到 http://localhost:10081/users/login
+  请求格式: { "userName": "joseph", "password": "123" }
+  其中 `那么打印Token` 将上一步 POST 请求的 response header 的 "Token" 项打印到日志
+  ---
+  作业2：熟悉 Selenium 使用
+  其中 `当在百度搜索关键字"cucumber"` 使用 Selenium 打开浏览器并通过百度搜索 (WebDriver 已经定义在 TestSteps.java 中)
+  其中 `那么打印百度为您找到的相关结果数` 使用 Selenium 从搜索结果页面抓取页面元素中的数据并打印到日志
+
+  场景: 作业1-打印登录Token
+    假如存在用户名为"joseph"和密码为"123"的用户
+    当通过API以用户名为"joseph"和密码为"123"登录时
+    那么打印Token
+
+  场景: 作业2-操作浏览器
+    当在百度搜索关键字"cucumber"
+    那么打印百度为您找到的相关结果数
+
+  场景: 环境测试
+    那么测试环境
