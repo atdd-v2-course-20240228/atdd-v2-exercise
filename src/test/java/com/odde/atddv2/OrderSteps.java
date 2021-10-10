0d3cc6539d170e2a37259ad523a044641d140822
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-10-10 15:52:59 +0800

    :fire: remove ui order list test and update api order list test

--- src/test/java/com/odde/atddv2/OrderSteps.java
+++ src/test/java/com/odde/atddv2/OrderSteps.java
@@ -1,12 +1,8 @@
 package com.odde.atddv2;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.odde.atddv2.entity.Order;
 import com.odde.atddv2.page.OrderPage;
 import com.odde.atddv2.page.WelcomePage;
-import com.odde.atddv2.repo.OrderRepo;
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.zh_cn.假如;
 import io.cucumber.java.zh_cn.当;
 import io.cucumber.java.zh_cn.那么;
 import lombok.SneakyThrows;
@@ -21,21 +17,7 @@ public class OrderSteps {
     private Browser browser;
 
     @Autowired
-    private OrderRepo orderRepo;
-
-    @Autowired
     private OrderPage orderPage;
-
-    @假如("存在如下订单:")
-    public void 存在如下订单(DataTable table) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        table.asMaps().forEach(map -> orderRepo.save(objectMapper.convertValue(map, Order.class)));
-    }
-
-    @当("查询订单时")
-    public void 查询订单时() {
-        welcomePage.goToOrders();
-    }
 
     @SneakyThrows
     @那么("显示如下订单")
@@ -45,7 +27,7 @@ public class OrderSteps {
 
     @当("用如下数据录入订单:")
     public void 用如下数据录入订单(DataTable table) {
-        查询订单时();
+        welcomePage.goToOrders();
         orderPage.addOrder(table.asMaps().get(0));
     }
 }
--- src/test/resources/api/order.feature
+++ src/test/resources/api/order.feature
@@ -3,12 +3,8 @@
 
   场景: 订单列表
     假如存在如下订单:
-      | code  | productName | total | recipientName | recipientMobile | recipientAddress | status        |
-      | SN001 | 电脑          | 19999 | 张三            | 13085901735     | 上海市长宁区           | toBeDelivered |
-    并且存在订单"SN001"的订单项:
-      | itemName | price | quantity |
-      | MacBook  | 19999 | 1        |
-      | TouchPad | 0     | 1        |
+      | code  | productName | total | recipientName | status        |
+      | SN001 | 电脑          | 19999 | 张三            | toBeDelivered |
     当API查询订单时
     那么返回如下订单
     """
@@ -16,18 +12,6 @@
         "code": "SN001",
         "productName": "电脑",
         "total": 19999,
-        "recipientName": "张三",
-        "recipientMobile": "13085901735",
-        "recipientAddress": "上海市长宁区",
-        "status": "toBeDelivered",
-        "lines": [{
-          "itemName": "MacBook",
-          "price": 19999,
-          "quantity": 1
-        }, {
-          "itemName": "TouchPad",
-          "price": 0,
-          "quantity": 1
-        }]
+        "status": "toBeDelivered"
       }]
     """
