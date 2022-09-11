75d42aedd5fc36b590a4931c5488521982df9a3f
Liangchen <leeonky@gmail.com>
2022-09-11 16:03:14 +0800

    Update test lib version

--- src/test/java/com/odde/atddv2/ApiOrderSteps.java
+++ src/test/java/com/odde/atddv2/ApiOrderSteps.java
@@ -72,5 +72,4 @@ public class ApiOrderSteps {
                 .withQueryStringParameter("type", "auto")
                 .withQueryStringParameter("number", deliverNo), json);
     }
-
 }
--- src/test/java/com/odde/atddv2/ApplicationSteps.java
+++ src/test/java/com/odde/atddv2/ApplicationSteps.java
@@ -1,5 +1,6 @@
 package com.odde.atddv2;
 
+import com.github.leeonky.cucumber.restful.RestfulStep;
 import com.odde.atddv2.repo.OrderRepo;
 import com.odde.atddv2.repo.UserRepo;
 import io.cucumber.java.Before;
@@ -7,6 +8,8 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.test.context.ContextConfiguration;
+
+import javax.annotation.PostConstruct;
 
 @ContextConfiguration(classes = {CucumberConfiguration.class}, loader = SpringBootContextLoader.class)
 @CucumberContextConfiguration
@@ -22,5 +25,13 @@ public class ApplicationSteps {
     public void clearDB() {
         userRepo.deleteAll();
         orderRepo.deleteAll();
+    }
+
+    @Autowired
+    private RestfulStep restfulStep;
+
+    @PostConstruct
+    public void setBaseUrl() {
+        restfulStep.setBaseUrl("http://127.0.0.1:10081/api/");
     }
 }
