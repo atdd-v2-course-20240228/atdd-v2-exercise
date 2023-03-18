3dfc034c3156dda1473c8a043a9e0b9d037633d3
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2023-03-18 11:55:27 +0800

    :sparkles: add homework for android

---
+++ src/test/java/com/odde/atddv2/AndroidSteps.java
@@ -0,0 +1,34 @@
+package com.odde.atddv2;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.cucumber.java.zh_cn.假如;
+import lombok.SneakyThrows;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AndroidSteps {
+    @SneakyThrows
+    @假如("输入用户名{string}")
+    public void 输入用户名(String name) {
+        AndroidDriver<AndroidElement> driver;
+        DesiredCapabilities caps = DesiredCapabilities.android();
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("udid", "emulator-5558");
+        caps.setCapability("platformVersion", "11.0");
+        caps.setCapability("platformName", "Android");
+//        caps.setCapability("app", "/root/tmp/app/app.apk");
+        caps.setCapability("app", "/home/androiduser/app-debug.apk");
+        caps.setCapability("remoteAdbHost", "anbox-mac");
+        driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), caps);
+        driver.launchApp();
+
+        TimeUnit.SECONDS.sleep(10);
+
+        // Input username in edit
+
+        driver.quit();
+    }
+}
